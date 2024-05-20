fff2ac27e9898826f6ee36d557042a2f146ea2d7
YonatanToker <yonatantoker123@gmail.com>
2024-05-20 17:13:06 +0300

    without LinkedList.ts and Jest files

--- LinkedList.ts
+++ LinkedList.ts
@@ -1,6 +1,6 @@
 class LinkedListNode<T> {
-  public value: T;
   public next: LinkedListNode<T> | null = null;
+  public value: T;
   constructor(value: T, next: LinkedListNode<T> | null) {
     this.value = value;
     this.next = next;
@@ -9,6 +9,7 @@ class LinkedListNode<T> {
 class LinkedList<T> {
   public head: LinkedListNode<T> | null = null;
   public length: number = 0;
+  // O(1) for insertion
   public insertAtHead = (value: T): void => {
     const newNode = new LinkedListNode(value, this.head);
     this.head = newNode;
--- app.ts
+++ app.ts
@@ -1,6 +1,7 @@
 import LinkedList from "./LinkedList";
 const ll = new LinkedList();
-ll.insertAtHead(10);
 ll.insertAtHead(20);
+console.log(ll);
+ll.insertAtHead(10);
 
 console.log(ll);
